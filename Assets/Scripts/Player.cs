cb4920c401283d481f492f32d742db17c975dfc9
ThanhToan <codehub.personal@gmail.com>
2024-08-22 15:07:21 +0700

    update roll

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public float rollBoost = 2f;
+    private float rollTime;
+    public float RollTime;
+    bool rollOnce = false;
+
+    private Rigidbody2D rb;
+
+    private Animator animator;
+
+
+    public Vector3 moveInput;
+
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        transform.position += moveSpeed * Time.deltaTime * moveInput;
+        
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if(Input.GetKeyDown(KeyCode.Space) && rollTime <= 0)
+        {
+            animator.SetBool("Roll", true);
+            moveSpeed += rollBoost;
+            rollTime = RollTime;
+            rollOnce = true;
+        }
+
+        if(rollTime <= 0  && rollOnce == true)
+        {
+            animator.SetBool("Roll", false);
+            moveSpeed -= rollBoost;
+            rollOnce = false;
+        }
+        else
+        {
+            rollTime -= Time.deltaTime;
+        }
+
+        if(moveInput.x != 0)
+        {
+            if (moveInput.x > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 0);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-1, 1, 0);
+            }
+        }
+
+
+    }
+}
